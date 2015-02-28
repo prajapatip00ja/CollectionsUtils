7a68c512f3ba5fb78f67f538b63bbfdf13ea1839
Pooja Prajapati <poojap@thoughtworks.com>
2015-02-28 16:07:16 +0530

    filter and reduce is working for generic

--- CollectionTest.java
+++ CollectionTest.java
@@ -5,16 +5,35 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-class IterableInt implements ListMapper<Integer>{
+class IterableInt implements ListMapper<Integer,Integer>{
     public Integer giveElement(Integer element){
         return element;
     };
 }
 
-class IterableStr implements ListMapper<String>{
+class FilterInt implements ListFilter<Integer>{
+     public Boolean filterElement(Integer element){
+        return (element%2==0);
+    };
+}
+
+class IterableStr implements ListMapper<String,String>{
     public String giveElement(String element){
         return element;
     };
+}
+
+class ConvertIntToStr implements ListMapper<Integer,String>{
+    public String giveElement(Integer element){
+        return element+"pooja";
+    }
+}
+
+class ReduceInt implements ListReducer<Integer,Integer>{
+    public Integer reducer(Integer element1 , Integer element2){
+       Integer i = element1.intValue()+element2.intValue();
+       return i;
+    }
 }
 
 
@@ -33,11 +52,13 @@ public class CollectionTest {
         list1.add(1);
         list1.add(2);
         list1.add(3);
-        List<Integer> list2 = CollectionsUtils.<Integer>map(list1,callback);
+        List<Integer> list2 = CollectionsUtils.<Integer,Integer>map(list1,callback);
         assertEquals(list2.get(0),(Integer)1);
         assertEquals(list2.get(1),(Integer)2);
         assertEquals(list2.get(2),(Integer)3);
     }
+
+    
 
     @Test
     public void TestMap_02(){
@@ -46,11 +67,50 @@ public class CollectionTest {
         list1.add("pooja");
         list1.add("dolly");
         list1.add("sayli");
-        List<Integer> list2 = CollectionsUtils.<String>map(list1,callback);
+        List<Integer> list2 = CollectionsUtils.<String,String>map(list1,callback);
         assertEquals(list2.get(0),(String)"pooja");
         assertEquals(list2.get(1),(String)"dolly");
         assertEquals(list2.get(2),(String)"sayli");
+    }
+
+    @Test
+    public void TestFilter_01(){
+        List<Integer> list1 = new ArrayList<Integer>();
+        ListFilter callback = new FilterInt();
+        list1.add(1);
+        list1.add(2);
+        list1.add(3);
+        List<Integer> list2 = CollectionsUtils.<Integer>filter(list1,callback);
+        assertEquals(list2.get(0),(Integer)2);
+        
     } 
+
+    @Test
+    public void TestMap_001(){
+        List<Integer> list1 = new ArrayList<Integer>();
+        ListMapper callback = new ConvertIntToStr();
+        list1.add(1);
+        list1.add(2);
+        list1.add(3);
+        List<String> list2 = CollectionsUtils.<Integer,String>map(list1,callback);
+        assertEquals(list2.get(0),(String)"1pooja");
+        assertEquals(list2.get(1),(String)"2pooja");
+        assertEquals(list2.get(2),(String)"3pooja");
+    }
+
+    @Test
+    public void TestReduce_001(){
+        List<Integer> list1 = new ArrayList<Integer>();
+        ListReducer callback = new ReduceInt();
+        Integer i = 5;
+        list1.add(1);
+        list1.add(2);
+        list1.add(3);
+        Integer list2 = CollectionsUtils.<Integer,Integer>reduce(list1,callback,i);
+        assertEquals(11,(int)list2);
+    }
+
+
 
 
 
--- CollectionsUtils.java
+++ CollectionsUtils.java
@@ -1,23 +1,50 @@
 import java.util.ArrayList;
 import java.util.List;
 
-interface ListMapper<E> {
-    public E giveElement(E element);
+interface ListMapper<E,K> {
+    public K giveElement(E element);
 }
 
+interface ListFilter<E> {
+	public Boolean filterElement(E element);
+}
+
+interface ListReducer<E,K> {
+	public K reducer(E element1, K element2);
+}
 
 
 public class CollectionsUtils{
 	
-	public static<E> List<E> map(List<E> list,ListMapper<E> callback){
+	public static<E,K> List<K> map(List<E> list,ListMapper<E,K> callback){
 
-		List<E> list1 = new ArrayList<E>();
+		List<K> list1 = new ArrayList<K>();
 
 		for(int i=0;i<list.size();i++){
 			list1.add(callback.giveElement(list.get(i)));
 		}
 
-		return list;
+		return list1;
+	}
+
+	public static<E> List<E> filter(List<E> list, ListFilter callback){
+		List<E> list1 = new ArrayList<E>();
+
+		for(int i=0;i<list.size();i++){
+			if((callback.filterElement(list.get(i)))==(Boolean)true){
+				list1.add(list.get(i));
+			}
+		}
+		return list1;	
+	}
+
+	public static<E,K> K reduce(List<E> list, ListReducer<E,K> callback, K intial){
+		
+		for(int i=0;i<list.size();i++){
+			intial = callback.reducer(list.get(i), intial);
+		}
+
+		return intial;
 	}
 
 }
